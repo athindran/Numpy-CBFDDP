e7a5eed40f4352e765652425deee040b44b4a5ea
Athindran Ramesh kumar <athindranrameshkumar@Athindrans-MacBook-Pro.local>
2024-12-18 20:54:07 -0500

    update files

--- dynamics.py
+++ dynamics.py
@@ -188,3 +188,78 @@ class Bicycle4D(gym.Env):
             return np.array([[-1.0, 0.1, 1.0, 0.2]])
         elif cbf_type=='D':
             return np.array([[-1.0, 0.1, 1.0, 0.2]])
+
+class Bicycle5D(gym.Env):
+    """
+    Bicycle5D System Environment
+    """
+    def __init__(self):
+        """
+        Initializes the state dimension, action dimension.
+        """
+        super(Bicycle5D, self).__init__()
+        self.state_dim = 5
+        self.action_dim = 2
+        self.wheel_base = 1.0
+        self.dt = 0.01
+
+    def step(self, obs, action=np.zeros((2, ))):
+        """
+        Integrates for one ODE step.
+        Action should be a one dimensional array of size action_dim.
+        Function additionally integrates internal state.
+        """
+        assert action.shape[0]==self.action_dim
+        action[0] = np.clip(action[0], a_min=-1.0, a_max=1.0)
+        action[1] = np.clip(action[1], a_min=-3.0, a_max=3.0)
+        # ODE solver
+        #print(obs, action)
+        ode_out = solve_ivp(fun=self.deriv_vec, y0=obs.ravel(), args=(action[np.newaxis, :]), t_span=(0, self.dt))
+        new_obs = ode_out.y[:, -1]
+        new_obs[2] = np.clip(new_obs[2], a_min=0.0, a_max=10.0)
+
+        return new_obs, action
+    
+    def get_jacobian(self, obs, action):
+        state_x = obs.ravel()
+        action = action.ravel()
+
+        A = np.array([[0.0, 0.0, np.cos(state_x[3]), -state_x[2]*np.sin(state_x[3]), 0.0], 
+                            [0.0, 0.0, np.sin(state_x[3]), state_x[2]*np.cos(state_x[3]), 0.0], 
+                            [0.0, 0.0, 0.0, 0.0, 0.0], 
+                            [0.0, 0.0, np.tan(state_x[4])/self.wheel_base, 0.0, state_x[2]/(1e-6 + self.wheel_base*np.cos(state_x[4])**2)],
+                            [0.0, 0.0, 0.0, 0.0, 0.0]])
+        B = np.array([[0.0, 0.0], [0.0, 0.0], [1.0, 0.0], [0.0, 0.0], [0.0, 1.0]])
+
+        Ad = np.eye(self.state_dim) + A*self.dt + 0.5*self.dt*self.dt*A@A
+
+        return Ad, self.dt*B, A, B
+    
+    def deriv_vec(self, t, state_x, u):
+        """
+        Return system forward propagation derivative.
+        """
+        state_x = state_x.ravel()
+        u = u.ravel()
+ 
+        deriv_vec = np.zeros((self.state_dim, ))
+        deriv_vec[0] = state_x[2]*np.cos(state_x[3])
+        deriv_vec[1] = state_x[2]*np.sin(state_x[3])
+        deriv_vec[2] = u[0]
+        deriv_vec[3] = state_x[2]*np.tan(state_x[4])/self.wheel_base
+        deriv_vec[4] = u[1]
+
+        return deriv_vec
+    
+    def reset(self, cbf_type):
+        """
+        Reset to initial state.
+        """
+        if cbf_type == 'A':
+            return np.array([[-1.0, 0.01, 3.0, 0.0, 0.0]])
+        elif cbf_type == 'B':
+            return np.array([[-1.0, 0.0, 3.0, 0.1, 0.0]])
+        elif cbf_type=='C':
+            return np.array([[-1.0, 0.1, 1.0, 0.2, 0.0]])
+        elif cbf_type=='D':
+            return np.array([[-1.0, 0.1, 1.0, 0.2, 0.0]])
